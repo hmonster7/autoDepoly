932e3689b955ea806dc900998cf53a1e68fef49d
monster <hmonster718@gmail.com>
2025-04-20 10:14:26 +0800

    add tests.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from .models import Item
 # Create your tests here.
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.post = Item.objects.create(
+            name="Test Post",
+            description="This is a test post",
+        )
+
+        def test_post_creation(self):
+            post = Item.objects.get(id=1)
+            self.assertEqual(post.name, "Test Post")
+            self.assertEqual(post.description, "This is a test post")
+
+        def test_post_created_auto_now_add(self):
+            post = Item.objects.get(id=1)
+            self.assertIsNotNone(post.created_at)
